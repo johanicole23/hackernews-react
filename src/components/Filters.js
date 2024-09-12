0fa38cd519a3e28bc2650019444aff52e0063735
johanicole23 <johaniky12@hotmail.com>
2024-09-12 00:44:38 -0500

    Corrections in the buttons and adding styles

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { Button, ButtonGroup } from '@mui/material';
+import { Button, ButtonGroup, ThemeProvider } from '@mui/material';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import { theme } from '../styles/myGeneralTheme';
+import personalizedStyles from '../styles/personalizedStyles';
 
 const Filters = ({ onFilter }) => {
   return (
-    <ButtonGroup variant="contained" aria-label="outlined primary button group">
-      <Button onClick={() => onFilter('normal')}>Normal Entries</Button>
-      <Button onClick={() => onFilter('lessThanFive')}>Less Than 5 Words</Button>
-      <Button onClick={() => onFilter('moreThanFive')}>More Than 5 Words</Button>
-    </ButtonGroup>
+    <ThemeProvider theme={theme}>
+      <ButtonGroup sx={{mb:'2rem'}}>
+        <Button
+          variant="contained"
+          onClick={() => onFilter('normal')}
+          sx={personalizedStyles.buttonFilter1}
+        > <RefreshIcon /></Button>
+        <Button
+          variant="contained"
+          onClick={() => onFilter('lessThanFive')}
+          sx={personalizedStyles.buttonFilter2}
+          startIcon={<FilterListIcon />}
+        > Less than 5</Button>
+        <Button
+          variant="contained"
+          onClick={() => onFilter('moreThanFive')}
+          sx={personalizedStyles.buttonFilter3}
+          startIcon={<FilterListIcon />}
+        >More Than 5 </Button>
+
+      </ButtonGroup>
+    </ThemeProvider>
+
   );
 };
 
