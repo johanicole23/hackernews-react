7bec0b2db0e98673591f59c9e8f4016b98912083
johanicole23 <johaniky12@hotmail.com>
2024-09-11 22:04:00 -0500

    Getting the list of the news with and withnot filters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { fetchEntries } from './services/apiService';
+import NewsTable from './components/NewsTable';
+import Filters from './components/Filters';
+import Loader from './components/Loader';
 
 function App() {
+  const [entries, setEntries] = useState({ normalEntries: [], lessThanFive: [], moreThanFive: [] });
+  const [filteredEntries, setFilteredEntries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getEntries = async () => {
+      const data = await fetchEntries();
+      if (data) {
+        setEntries(data);
+        setFilteredEntries(data.normalEntries);  // Default entries
+      }
+      setLoading(false);
+    };
+
+    getEntries();
+  }, []);
+
+  const handleFilter = (filterType) => {
+    if (filterType === 'lessThanFive') {
+      setFilteredEntries(entries.lessThanFive);
+    } else if (filterType === 'moreThanFive') {
+      setFilteredEntries(entries.moreThanFive);
+    } else {
+      setFilteredEntries(entries.normalEntries); // Default
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? <Loader /> : (
+        <>
+          <Filters onFilter={handleFilter} />
+          <NewsTable entries={filteredEntries} />
+        </>
+      )}
     </div>
   );
 }
